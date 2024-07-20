0fe18098f9eeaf5c95a87d24d0295c8083dd6943
Mack Octavian <maestrooctavian@gmail.com>
2024-07-20 23:39:16 +0300

    Added radix ui icons for some components

--- src/ResetButton.jsx
+++ src/ResetButton.jsx
@@ -1,6 +1,10 @@
+import {ResetIcon} from '@radix-ui/react-icons' 
+
 const ResetButton = () => {
   return (
-    <button className="reset-btn">Reset</button>
+    <button className="reset-btn">
+        <ResetIcon></ResetIcon>
+    </button>
   )
 }
 
