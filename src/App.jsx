418ac3345eaca5530c0bd1375f55fc219a17e770
Mack Octavian <maestrooctavian@gmail.com>
2024-07-21 02:15:16 +0300

    Prop Drilling using children prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,44 @@
-import { useEffect, useState } from "react"
-import { Count } from "./Count"
-import ResetButton from "./ResetButton"
-import Title from "./Title"
-import ButtonContainer from "./ButtonContainer"
-import CountButton from "./CountButton"
-
+import { useEffect, useState } from "react";
+import { Count } from "./Count";
+import ResetButton from "./ResetButton";
+import Title from "./Title";
+import ButtonContainer from "./ButtonContainer";
+import CountButton from "./CountButton";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const lock = count === 5 ? true : false
+  const [count, setCount] = useState(0);
+  const lock = count === 5 ? true : false;
 
   useEffect(() => {
     const handleKeyDown = (e) => {
-      if (e.code === 'Space') { 
-        const newCount = count + 1
+      if (e.code === "Space") {
+        const newCount = count + 1;
         if (newCount > 5) {
-          return setCount(5)
-          
+          return setCount(5);
         }
 
-        setCount(newCount)
+        setCount(newCount);
       }
-    }
-    window.addEventListener('keydown', handleKeyDown)
+    };
+    window.addEventListener("keydown", handleKeyDown);
     return () => {
-      window.removeEventListener("keydown", handleKeyDown)
-    }
-   
-  }, [count])
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [count]);
 
   return (
     <main>
-      <div className={`card ${lock ? 'card--limit' : ""}`}>
-      <Title lock={lock}/>
-      <Count count={count}/>
-      <ResetButton setCount={setCount}/>
-      <ButtonContainer>
-      <CountButton type="minus" setCount={setCount} lock={lock}/>
-      <CountButton type="plus" setCount={setCount} lock={lock}/>
-      </ButtonContainer>
+      <div className={`card ${lock ? "card--limit" : ""}`}>
+        <Title lock={lock} />
+        <Count count={count} />
+        <ResetButton setCount={setCount} />
+        <ButtonContainer>
+          <CountButton type="minus" setCount={setCount} lock={lock} />
+          <CountButton type="plus" setCount={setCount} lock={lock} />
+        </ButtonContainer>
       </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
